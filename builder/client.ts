abaff1c185078abdc12723be7f7e80b0c50b0953
Linjie <linjielig@gmail.com>
2021-10-30 22:23:33 +0400

    add builder pattern

---
+++ builder/client.ts
@@ -0,0 +1,22 @@
+/// <reference path="builder.ts" />
+
+function clientCode(director: Builder.Director) {
+    const builder = new Builder.ConcreteBuilder1();
+    director.setBuilder(builder);
+
+    console.log('Standard basic product:');
+    director.buildMinimalViableProduct();
+    builder.getProduct().listParts();
+
+    console.log("Standard full featured product:");
+    director.buildFullFeaturedProduct();
+    builder.getProduct().listParts();
+
+    console.log('Custom product:');
+    builder.producePartA();
+    builder.producePartC();
+    builder.getProduct().listParts();
+}
+
+const director = new Builder.Director();
+clientCode(director);
